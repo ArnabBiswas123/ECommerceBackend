02974503ea26beea4ac4b137b6b171e7dad70e59
ArnabBiswas123 <iamarnabbiswas2001@gmail.com>
2025-05-30 21:57:26 +0530

    first commit

--- controllers/stripeWebhookController.js
+++ controllers/stripeWebhookController.js
@@ -7,7 +7,7 @@ const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
 const stripeWebhookController = async (req, res) => {
   const sig = req.headers['stripe-signature'];
-
+ 
   let event;
   try {
     event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
@@ -15,7 +15,7 @@ const stripeWebhookController = async (req, res) => {
     console.error('⚠️  Webhook signature verification failed:', err.message);
     return res.status(400).send(`Webhook Error: ${err.message}`);
   }
-
+console.log(event)
  
   if (event.type === 'checkout.session.completed') {
     const session = event.data.object;
